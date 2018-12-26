271324a4e5c80fda65859fd9803dc5430e134ec4
Craig <craig@craigrueda.com>
2018-12-25 20:40:04 -0800

    Cleaning up app/ts

--- src/app.ts
+++ src/app.ts
@@ -5,21 +5,14 @@ import dotenv from "dotenv";
 import path from "path";
 
 // Controllers (route handlers)
-// import * as homeController from "./controllers/home";
-// import * as userController from "./controllers/user";
 import * as slackController from "./controllers/slackController";
-// import * as contactController from "./controllers/contact";
 
-
-// API keys and Passport configuration
-// import * as passportConfig from "./config/passport";
 
 // Create Express server
 const app = express();
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
-app.set("views", path.join(__dirname, "../views"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
